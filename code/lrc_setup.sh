d9b1c3b79e42b72b6347190a6bee121f848af999
Neil Zhao <neil@zzrcxb.me>
2026-01-12 16:55:47 -0600

    Added the check for the existing .vscode-server directory

--- code/lrc_setup.sh
+++ code/lrc_setup.sh
@@ -3,11 +3,17 @@
 set -eu -o pipefail
 
 SERVER_PATH=/misc/scratch/$(whoami)/.vscode-server
+DEFAULT_PATH=$HOME/.vscode-server
+
+if [ -d $DEFAULT_PATH ]; then
+    echo "Found .vscode-server directory at $DEFAULT_PATH. Aborting"
+    exit 0
+fi
 
 echo "Path to vscode server folder: $SERVER_PATH"
 
 mkdir -p $SERVER_PATH
 
-ln -s $SERVER_PATH $HOME/.vscode-server
+ln -s $SERVER_PATH $DEFAULT_PATH
 
 echo "Symbolic link set"
